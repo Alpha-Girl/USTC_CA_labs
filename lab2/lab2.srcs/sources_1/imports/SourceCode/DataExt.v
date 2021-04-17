f1e560d699533455065b3c43554430f3eeeb9cf9
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2021-04-17 23:20:20 +0800

    create lab2

---
+++ lab2/lab2.srcs/sources_1/imports/SourceCode/DataExt.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: USTC ESLAB 
+// Engineer: Wu Yuzhang
+// 
+// Design Name: RISCV-Pipline CPU
+// Module Name: DataExt 
+// Target Devices: Nexys4
+// Tool Versions: Vivado 2017.4.1
+// Description: Data Extension module
+//////////////////////////////////////////////////////////////////////////////////
+//功能说明
+    //DataExt是用来处理非字对齐load的情形，同时根据load的不同模式对Data Mem中load的数进行符号或者无符号拓展，组合逻辑电路
+//输入
+    //IN                    是从Data Memory中load的32bit字
+    //LoadedBytesSelect     等价于AluOutM[1:0]，是读Data Memory地址的低两位，
+                            //因为DataMemory是按字（32bit）进行访问的，所以需要把字节地址转化为字地址传给DataMem
+                            //DataMem一次返回一个字，低两位地址用来从32bit字中挑选出我们需要的字节
+    //RegWriteW             表示不同的 寄存器写入模式 ，所有模式定义在Parameters.v中
+//输出
+    //OUT表示要写入寄存器的最终值
+//实验要求  
+    //补全模块  
+
+`include "Parameters.v"   
+module DataExt(
+    input wire [31:0] IN,
+    input wire [1:0] LoadedBytesSelect,
+    input wire [2:0] RegWriteW,
+    output reg [31:0] OUT
+    );    
+        
+    // 请补全此处代码
+
+endmodule
+
---
+++ lab2/lab2.srcs/sources_1/imports/SourceCode/NPC_Generator.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: USTC ESLAB 
+// Engineer: Wu Yuzhang
+// 
+// Design Name: RISCV-Pipline CPU
+// Module Name: NPC_Generator
+// Target Devices: Nexys4
+// Tool Versions: Vivado 2017.4.1
+// Description: Choose Next PC value
+//////////////////////////////////////////////////////////////////////////////////
+//功能说明
+    //NPC_Generator是用来生成Next PC值的模块，根据不同的跳转信号选择不同的新PC值
+//输入
+    //PCF              旧的PC值
+    //JalrTarget       jalr指令的对应的跳转目标
+    //BranchTarget     branch指令的对应的跳转目标
+    //JalTarget        jal指令的对应的跳转目标
+    //BranchE==1       Ex阶段的Branch指令确定跳转
+    //JalD==1          ID阶段的Jal指令确定跳转
+    //JalrE==1         Ex阶段的Jalr指令确定跳转
+//输出
+    //PC_In            NPC的值
+//实验要求  
+    //补全模块  
+
+module NPC_Generator(
+    input wire [31:0] PCF,JalrTarget, BranchTarget, JalTarget,
+    input wire BranchE,JalD,JalrE,
+    output reg [31:0] PC_In
+    );
+    
+    // 请补全此处代码
+    
+endmodule
